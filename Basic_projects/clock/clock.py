aafd326e945eabc3b1f940ebd87409905cd43958
Kailash Bora <kailashbora007@gmail.com>
2021-08-04 09:32:43 +0530

    Created basic gui application of clock and calculator using tkinter

---
+++ Basic_projects/clock/clock.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from time import *
+
+
+def update():
+    time_string = strftime("%H:%M:%S %p")
+    time_label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
+
+    time_label.after(1000, update)
+
+
+window = Tk()
+window.title("CLOCK")
+
+
+time_label = Label(window, font=("Arial", 50), fg="gray", bg="white")
+time_label.pack()
+
+day_label = Label(window, font=("Ink Free", 25), fg="gray", bg="white")
+day_label.pack()
+
+date_label = Label(window, font=("Ink Free", 25), fg="gray", bg="white")
+date_label.pack()
+
+update()
+
+
+window.mainloop()
